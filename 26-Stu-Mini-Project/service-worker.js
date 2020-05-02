94c5f960a3cf52d8e0460f27ae3f7c02eaa2b4ef
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-01 22:56:21 -0400

    added an event listener to service worker

--- 26-Stu-Mini-Project/service-worker.js
+++ 26-Stu-Mini-Project/service-worker.js
@@ -6,3 +6,27 @@ const FILES_TO_CACHE = [
     "/dist.topic.bundle.js"
 ];
 
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+// The activate handler that takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheName => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
