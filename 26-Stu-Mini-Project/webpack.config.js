e5cba8effe5d2fa4bf06fcbe1418a94a9c180c02
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-03 17:25:50 -0400

    added a webpack plugin

--- 26-Stu-Mini-Project/webpack.config.js
+++ 26-Stu-Mini-Project/webpack.config.js
@@ -25,7 +25,20 @@ const config = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            filename: "manifest.json",
+            inject: false,
+            fingerprints: false, 
+            name: "PWA Budget App",
+            short_name: "Budget App",
+            description: "An application to track your budget. Also works as a PWA",
+            background_color: "#C3EDEA",
+            theme_color: "#ffffff",
+            start_url: "/"
+        })
+    ]
 };
 
 module.exports = config;
